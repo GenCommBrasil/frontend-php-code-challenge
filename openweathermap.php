276a902f9ffabd6338d29c421a7cdd3a284e9658
Rigote <matheus.rigote@rakuten.com.br>
2018-03-05 19:44:27 -0300

    Iniciando plugin

---
+++ openweathermap.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Openweathermap
+Plugin URI: https://github.com/rigote/frontend-php-code-challenge
+Description: Plugin with the aim of displaying the current weather to any place on Earth using geolocation
+Version: 1.0
+Author: Matheus Souza Rigote
+Author URI: http://www.rigotedesign.com.br
+License: GPLv2
+*/
+
+class Openweathermap() {
+    
+}
