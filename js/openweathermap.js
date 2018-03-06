2940e82a2597f1af6bca525746c378654ce3c333
Matheus Rigote <matheus@rigotedesign.com.br>
2018-03-06 02:33:50 -0300

    Configurações do plugin no php, implementação via shortcode

--- js/openweathermap.js
+++ js/openweathermap.js
@@ -1,18 +1,39 @@
 (function(){
     var apiKey = 'bf6c435b393365c41783818f1e966892';
 
+    function weatherTemplate(res) {
+        var currentLocation = res.name;
+        var weatherNow = res.weather[0].main;
+        var icon = res.weather[0].icon;
+        var temp = res.main.temp;
+        var minTemp = res.main.temp_min;
+        var maxTemp = res.main.temp_max;
+        var pressure = res.main.pressure;
+        var humidity = res.main.humidity;
+    
+        var urlIcon = "../images/" + icon + ".png";
+    
+        jQuery('#icon').prepend('<img src=' + urlIcon + ' width="80" height="80">');
+        jQuery('#currentLocation').html(currentLocation);
+        jQuery('#temp').html(Math.round(temp) + '°C');
+        jQuery('#minTemp').html('<span>Mínima</span>' + minTemp + '°C');
+        jQuery('#maxTemp').html('<span>Máxima</span>' + maxTemp + '°C');
+        jQuery('#pressure').html('<span>Pressão</span>' + pressure + 'hPa');
+        jQuery('#humidity').html('<span>Humidade</span>' + humidity + '%');
+    }
+
     function onPositionReceived(position){
         var lat = position.coords.latitude;
         var long = position.coords.longitude;
 
-        $.getJSON('http://api.openweathermap.org/data/2.5/weather?lat='+ lat +'&lon='+ long +'&APPID='+ apiKey +'&units=metric', function(result){
+        jQuery.getJSON('http://api.openweathermap.org/data/2.5/weather?lat='+ lat +'&lon='+ long +'&APPID='+ apiKey +'&units=metric', function(result){
             console.log(result);
             weatherTemplate(result);
         });
     }
 
     function locationNotReceived(positionError){
-        $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Sao Paulo,BR&APPID='+ apiKey +'&units=metric', function(result){
+        jQuery.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Sao Paulo,BR&APPID='+ apiKey +'&units=metric', function(result){
             console.log(result);
             weatherTemplate(result);
         });
--- openweathermap.php
+++ openweathermap.php
@@ -9,6 +9,42 @@ Author URI: http://www.rigotedesign.com.br
 License: GPLv2
 */
 
-class Openweathermap() {
-    
+class Openweathermap {
+    private static $instance;
+
+    public static function getInstance(){
+        if(self::$instance == NULL){
+            self::$instance = new self();
+        }
+    }
+
+    private function __construct(){
+        add_action( 'wp_enqueue_scripts', 'registerPluginScripts' );
+        add_shortcode( 'weathermap', 'showWeathermap' );
+    }
 }
+
+function registerPluginScripts(){
+    wp_register_style('openweathermap', plugin_dir_url(__FILE__).'/css/style.css');
+    wp_register_script( 'openweathermap', plugins_url( '/js/openweathermap.js', __FILE__ ), 'jquery', 1.0, true);
+    wp_register_script( 'openweathermap', plugins_url( '/js/weatherTemplate.js', __FILE__ ), 'jquery', 1.0, true);
+    wp_enqueue_style('openweathermap');
+    wp_enqueue_script( 'openweathermap' );
+}
+
+function showWeathermap() {
+    echo '<div id="weather">';
+    echo '   <div id="currentLocation"></div>';
+    echo '      <div id="icon">';
+    echo '      <div id="temp"></div>';
+    echo '   </div>';
+    echo '   <div class="prevent">';
+    echo '      <div id="minTemp"></div>';
+    echo '      <div id="maxTemp"></div>';
+    echo '      <div id="humidity"></div>';
+    echo '      <div id="pressure"></div>';
+    echo '   </div>';
+    echo '</div>';
+  }
+
+Openweathermap::getInstance();
