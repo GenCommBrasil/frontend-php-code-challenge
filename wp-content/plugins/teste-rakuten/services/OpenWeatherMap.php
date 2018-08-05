928a1ece379b9be5c6ab9f6b7a68fe3dbbd12ad3
Alex Silva <alexsilva.info@gmail.com>
2018-08-05 12:49:52 -0300

    created service for openweathermap with interface location and file location php for request post

--- wp-content/plugins/teste-rakuten/services/OpenWeatherMap.php
+++ wp-content/plugins/teste-rakuten/services/OpenWeatherMap.php
@@ -1,7 +1,7 @@
 <?php
 
-include_once($_SERVER["DOCUMENT_ROOT"] . "/config/Parameters.php");
-include_once($_SERVER["DOCUMENT_ROOT"] . "/helper/TemperatureHelper.php");
+include_once(__DIR__ . "/../config/Parameters.php");
+include_once(__DIR__ . "/../helper/TemperatureHelper.php");
 include_once("Location.php");
 
 /**
